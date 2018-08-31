5453346fccf439eca93116197cbbd87d0dd01449
Khrystyns Khozhai <l.xrustja@gmail.com>
2018-08-31 17:51:33 +0300

    responsive for 1024px

--- js/main-js.js
+++ js/main-js.js
@@ -14,14 +14,44 @@ $(document).ready(function() {
         arrows: true,
         // autoplay: true,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                
+              }
+            }
+          ]
       });
       $('.slider-news').slick({
         infinite: true,
         arrows: true,
         // autoplay: true,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                
+              }
+            }
+          ]
       });
 	
 });
