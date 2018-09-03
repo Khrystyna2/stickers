56c56ccc66eecb938afb7f92e93e71c8772cf082
Khrystyns Khozhai <l.xrustja@gmail.com>
2018-09-03 16:59:45 +0300

    added modal window and snb-menu and adaptive for 768px

--- js/main-js.js
+++ js/main-js.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+
+  // if ($(window).width() > 1024) {
+  //   $('.menu-dropdown').hover(function () {
+  //     $(this).find('.wrapper-sub-menu').fadeIn(200);
+  //   }, function () {
+  //     $('.wrapper-sub-menu').fadeOut(200);
+  //   });
+  // }
+  // if ($(window).width() <= 1024) {
+  //   $('.top-menu').click(function () {
+  //     $('.wrapper-sub-menu').slideToggle(500);
+  //   });
+  // }
+
+    $('.menu-dropdown').click(function () {
+      $(this).find('.wrapper-sub-menu').slideToggle(400);
+    });
+
+
     
     $('.top-slider').slick({
         infinite: true,
