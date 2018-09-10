296c2ebeaddbbbdfec69ef810dbd074e4b934994
Khrystyns Khozhai <l.xrustja@gmail.com>
2018-09-10 10:10:05 +0300

    added new photo and modified blog page

--- js/main-js.js
+++ js/main-js.js
@@ -215,4 +215,36 @@ $(document).ready(function() {
         $('.slider-card').slick('slickGoTo', goToSingleSlide);
       });
 
+
+      $('.slider-blog').slick({
+        infinite: true,
+        arrows: true,
+        // autoplay: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      });
+
 });
