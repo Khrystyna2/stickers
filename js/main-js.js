9aa196396876e83bb2c19e7ab24a2c75d75509df
Khrystyns Khozhai <l.xrustja@gmail.com>
2018-09-13 12:36:51 +0300

    added new page(card-goods-3 and card-goods-4)

--- js/main-js.js
+++ js/main-js.js
@@ -305,4 +305,41 @@ $(document).ready(function() {
         });
 
 
+        $('.slider-card-goods').slick({
+          infinite: true,
+          arrows: true,
+          // autoplay: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          responsive: [
+              {
+                breakpoint: 992,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 767,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 420,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+        });
 });
